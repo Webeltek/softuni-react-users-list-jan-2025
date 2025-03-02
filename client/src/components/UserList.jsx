a55fe3e463657a824e24c436ec343ae7addf185f
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2025-03-02 23:45:02 +0100

    Extract more components

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,62 +1,133 @@
+import Pagination from "./Pagination";
+import Search from "./Search";
+import UserListItem from "./UserListItem";
+
 export default function UserList(){
     return (
-    <section className="card users-container">
-        {/* <!-- Search bar component --> */}
-        
+<section className="card users-container">
+      {/* <!-- Search bar component --> */}
+      <Search />
 
-        {/* <!-- New user button  --> */}
-        <button className="btn-add btn">Add new user</button>
+      {/* <!-- Table component --> */}
+      <div className="table-wrapper">
+        <div className="overlays">
 
-        {/* <!-- Pagination component  --> */}
-        <div className="pagination position">
-            <div className="limits">
-            <span>Items per page:</span>
-            <select name="limit" className="limit" value="5">
-                <option value="5">5</option>
-                <option value="5">10</option>
-                <option value="5">15</option>
-                <option value="5">20</option>
-            </select>
-            </div>
-            <p className="pages">1 - 1 of 1</p>
-            <div className="actions">
-            <button className="btn" title="First Page">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angles-left"
-                className="svg-inline--fa fa-angles-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <path fill="currentColor"
-                    d="M77.25 256l137.4-137.4c12.5-12.5 12.5-32.75 0-45.25s-32.75-12.5-45.25 0l-160 160c-12.5 12.5-12.5 32.75 0 45.25l160 160C175.6 444.9 183.8 448 192 448s16.38-3.125 22.62-9.375c12.5-12.5 12.5-32.75 0-45.25L77.25 256zM269.3 256l137.4-137.4c12.5-12.5 12.5-32.75 0-45.25s-32.75-12.5-45.25 0l-160 160c-12.5 12.5-12.5 32.75 0 45.25l160 160C367.6 444.9 375.8 448 384 448s16.38-3.125 22.62-9.375c12.5-12.5 12.5-32.75 0-45.25L269.3 256z">
-                </path>
-                </svg>
-            </button>
+        {/* <!-- Overlap components  --> */}
 
-            <button className="btn" title="Previous Page">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left"
-                className="svg-inline--fa fa-angle-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
-                <path fill="currentColor"
-                    d="M192 448c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l137.4 137.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448z">
-                </path>
-                </svg>
-            </button>
-            <button className="btn" title="Next Page">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right"
-                className="svg-inline--fa fa-angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
-                <path fill="currentColor"
-                    d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z">
-                </path>
-                </svg>
-            </button>
+        {/* <!-- <div className="loading-shade"> --> */}
+        {/* <!-- Loading spinner  --> */}
+        {/* <!-- <div className="spinner"></div> --> */}
+        {/* <!--  */}
+        {/* No users added yet  --> */}
 
-            <button className="btn" title="Last Page">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angles-right"
-                className="svg-inline--fa fa-angles-right" role="img" xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512">
-                <path fill="currentColor"
-                    d="M246.6 233.4l-160-160c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25L178.8 256l-137.4 137.4c-12.5 12.5-12.5 32.75 0 45.25C47.63 444.9 55.81 448 64 448s16.38-3.125 22.62-9.375l160-160C259.1 266.1 259.1 245.9 246.6 233.4zM438.6 233.4l-160-160c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25L370.8 256l-137.4 137.4c-12.5 12.5-12.5 32.75 0 45.25C239.6 444.9 247.8 448 256 448s16.38-3.125 22.62-9.375l160-160C451.1 266.1 451.1 245.9 438.6 233.4z">
-                </path>
-                </svg>
-            </button>
-            </div>
+        {/* <div className="table-overlap">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="triangle-exclamation"
+                className="svg-inline--fa fa-triangle-exclamation Table_icon__+HHgn"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"
+                ></path>
+              </svg>
+              <h2>There is no users yet.</h2>
+            </div>  */}
+
+
+        {/* <!-- On error overlap component  --> */}
+
+            {/* <div className="table-overlap">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="triangle-exclamation"
+                className="svg-inline--fa fa-triangle-exclamation Table_icon__+HHgn"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"
+                ></path>
+              </svg>
+              <h2>Failed to fetch</h2>
+            </div> */}
         </div>
-        </section>        
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th>
+                Image
+              </th>
+              <th>
+                First name<svg aria-hidden="true" focusable="false" data-prefix="fas"
+                  data-icon="arrow-down" className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img"
+                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                  <path fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                  </path>
+                </svg>
+              </th>
+              <th>
+                Last name<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img" xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512">
+                  <path fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                  </path>
+                </svg>
+              </th>
+              <th>
+                Email<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img" xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512">
+                  <path fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                  </path>
+                </svg>
+              </th>
+              <th>
+                Phone<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img" xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512">
+                  <path fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                  </path>
+                </svg>
+              </th>
+              <th>
+                Created
+                <svg aria-hidden="true" focusable="false" data-prefix="fas"
+                  data-icon="arrow-down" className="icon active-icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img"
+                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                  <path fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                  </path>
+                </svg>
+              </th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <UserListItem />
+          </tbody>
+        </table>
+      </div>
+
+      {/* <!-- New user button  --> */}
+      <button className="btn-add btn">Add new user</button>
+
+
+      <Pagination />
+    </section>        
     )
 }
